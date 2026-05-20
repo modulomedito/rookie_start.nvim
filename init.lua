299ad1977a6c5b8feed208539c5976fc3c7bfd08
modulomedito <chcchc1995@outlook.com>
2026-05-21 07:47:58 +0800

    🦄 refactor(config):[#]restructure init.lua
    - add .vscode to .gitignore to exclude VS Code workspace files
    - restructure init.lua: rename section headers and TOC entries, condense inline comments, add MY_NOTES section with full config documentation

--- init.lua
+++ init.lua
@@ -1,98 +1,46 @@
 -- =================================================================================================
--- Load your secret
+-- YOUR_SECRET
 -- =================================================================================================
 require("secret")
 
 -- =================================================================================================
--- TABLE OF CONTENTS
+-- TABLE_OF_CONTENTS
 -- =================================================================================================
--- OPTIONS
--- KEYMAPS
--- AUTOCMDS
--- PLUGIN_MANAGER
-
--- =================================================================================================
--- OPTIONS
--- =================================================================================================
--- Set <space> as the leader key
--- See `:help mapleader`
--- Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
-
--- Make line numbers default
-vim.o.number = true
-
--- Enable mouse mode, can be useful for resizing splits for example!
-vim.o.mouse = "a"
-
--- Don't show the mode, since it's already in the status line
-vim.o.showmode = false
-
--- Sync clipboard between OS and Neovim.
--- Schedule the setting after `UiEnter` because it can increase startup-time.
--- Remove this option if you want your OS clipboard to remain independent.
--- See `:help 'clipboard'`
-vim.schedule(function()
-    vim.o.clipboard = "unnamedplus"
-end)
-
--- Enable break indent
-vim.o.breakindent = true
-
--- Enable undo/redo changes even after closing and reopening a file
-vim.o.undofile = true
-
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.o.signcolumn = "yes"
-
--- Decrease update time
-vim.o.updatetime = 250
-
--- Decrease mapped sequence wait time
-vim.o.timeoutlen = 300
-
--- Configure how new splits should be opened
-vim.o.splitright = true
-vim.o.splitbelow = true
-
--- Sets how neovim will display certain whitespace characters in the editor.
--- See `:help 'list'`
--- and `:help 'listchars'`
---
--- Notice listchars is set using `vim.opt` instead of `vim.o`.
--- It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
--- See `:help lua-options`
--- and `:help lua-guide-options`
-vim.o.list = true
-vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
-
--- Preview substitutions live, as you type!
-vim.o.inccommand = "split"
-
--- Show which line your cursor is on
-vim.o.cursorline = true
-
--- Show which column your cursor is on
-vim.o.cursorcolumn = true
-
--- Minimal number of screen lines to keep above and below the cursor.
-vim.o.scrolloff = 10
-
--- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s)
--- See `:help 'confirm'`
-vim.o.confirm = true
+-- NVIM_OPTIONS
+-- NVIM_KEYMAPS
+-- NVIM_AUTOCMDS
+-- NVIM_PLUGIN_MANAGER
+-- MY_NOTES
 
 -- =================================================================================================
--- KEYMAPS
+-- NVIM_OPTIONS
+-- =================================================================================================
+vim.g.mapleader = " " -- See NOTES_OPTIONS_LEADER
+vim.g.maplocalleader = " " -- See NOTES_OPTIONS_LEADER
+vim.g.have_nerd_font = false -- See NOTES_OPTIONS_NERD_FONT
+vim.o.number = true -- See NOTES_OPTIONS_NUMBER
+vim.o.mouse = "a" -- See NOTES_OPTIONS_MOUSE
+vim.o.showmode = false -- See NOTES_OPTIONS_SHOWMODE
+vim.schedule(function() vim.o.clipboard = "unnamedplus" end) -- See NOTES_OPTIONS_CLIPBOARD
+vim.o.breakindent = true -- See NOTES_OPTIONS_BREAKINDENT
+vim.o.undofile = true -- See NOTES_OPTIONS_UNDOFILE
+vim.o.ignorecase = true -- See NOTES_OPTIONS_IGNORECASE
+vim.o.smartcase = true -- See NOTES_OPTIONS_SMARTCASE
+vim.o.signcolumn = "yes" -- See NOTES_OPTIONS_SIGNCOLUMN
+vim.o.updatetime = 250 -- See NOTES_OPTIONS_UPDATETIME
+vim.o.timeoutlen = 300 -- See NOTES_OPTIONS_TIMEOUTLEN
+vim.o.splitright = true -- See NOTES_OPTIONS_SPLITRIGHT
+vim.o.splitbelow = true -- See NOTES_OPTIONS_SPLITBELOW
+vim.o.list = true -- See NOTES_OPTIONS_LIST
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" } -- See NOTES_OPTIONS_LISTCHARS
+vim.o.inccommand = "split" -- See NOTES_OPTIONS_INCCOMMAND
+vim.o.cursorline = true -- See NOTES_OPTIONS_CURSORLINE
+vim.o.cursorcolumn = true -- See NOTES_OPTIONS_CURSORCOLUMN
+vim.o.scrolloff = 10 -- See NOTES_OPTIONS_SCROLLOFF
+vim.o.confirm = true -- See NOTES_OPTIONS_CONFIRM
+
+-- =================================================================================================
+-- NVIM_KEYMAPS
 -- =================================================================================================
 -- See `:help vim.keymap.set()`
 -- Clear highlights on search when pressing <Esc> in normal mode
@@ -108,11 +56,11 @@ vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower win
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 -- =================================================================================================
--- AUTOCMDS
+-- NVIM_AUTOCMDS
 -- =================================================================================================
 
 -- =================================================================================================
--- PLUGIN_MANAGER
+-- NVIM_PLUGIN_MANAGER
 -- =================================================================================================
 -- Install the lazy.nvim plugin manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
@@ -1013,3 +961,84 @@ vim.list_extend(plugin_cfg, cfg_which_key_nvim)
 --     },
 -- })
 --
+
+-- =================================================================================================
+-- MY_NOTES
+-- =================================================================================================
+-- NOTES_OPTIONS_LEADER
+--     Set <space> as the leader key
+--     See `:help mapleader`
+--     Must happen before plugins are loaded (otherwise wrong leader will be used)
+--
+-- NOTES_OPTIONS_NERD_FONT
+--     Set to true if you have a Nerd Font installed and selected in the terminal
+--
+-- NOTES_OPTIONS_NUMBER
+--     Make line numbers default
+--
+-- NOTES_OPTIONS_MOUSE
+--     Enable mouse mode, can be useful for resizing splits for example!
+--
+-- NOTES_OPTIONS_SHOWMODE
+--     Don't show the mode, since it's already in the status line
+--
+-- NOTES_OPTIONS_CLIPBOARD
+--     Sync clipboard between OS and Neovim.
+--     Schedule the setting after `UiEnter` because it can increase startup-time.
+--     Remove this option if you want your OS clipboard to remain independent.
+--     See `:help 'clipboard'`
+--
+-- NOTES_OPTIONS_BREAKINDENT
+--     Enable break indent
+--
+-- NOTES_OPTIONS_UNDOFILE
+--     Enable undo/redo changes even after closing and reopening a file
+--
+-- NOTES_OPTIONS_IGNORECASE
+--     Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+--
+-- NOTES_OPTIONS_SMARTCASE
+--     Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+--
+-- NOTES_OPTIONS_SIGNCOLUMN
+--     Keep signcolumn on by default
+--
+-- NOTES_OPTIONS_UPDATETIME
+--     Decrease update time
+--
+-- NOTES_OPTIONS_TIMEOUTLEN
+--     Decrease mapped sequence wait time
+--
+-- NOTES_OPTIONS_SPLITRIGHT
+--     Configure how new splits should be opened
+--
+-- NOTES_OPTIONS_SPLITBELOW
+--     Configure how new splits should be opened
+--
+-- NOTES_OPTIONS_LIST
+--     Sets how neovim will display certain whitespace characters in the editor.
+--     See `:help 'list'`
+--     and `:help 'listchars'`
+--
+-- NOTES_OPTIONS_LISTCHARS
+--     Notice listchars is set using `vim.opt` instead of `vim.o`.
+--     It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--     See `:help lua-options`
+--     and `:help lua-guide-options`
+--
+-- NOTES_OPTIONS_INCCOMMAND
+--     Preview substitutions live, as you type!
+--
+-- NOTES_OPTIONS_CURSORLINE
+--     Show which line your cursor is on
+--
+-- NOTES_OPTIONS_CURSORCOLUMN
+--     Show which column your cursor is on
+--
+-- NOTES_OPTIONS_SCROLLOFF
+--     Minimal number of screen lines to keep above and below the cursor.
+--
+-- NOTES_OPTIONS_CONFIRM
+--     if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+--     instead raise a dialog asking if you wish to save the current file(s)
+--     See `:help 'confirm'`
