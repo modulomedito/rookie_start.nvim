0e19a29ba605acab68c09b030f0b6d34acf38492
modulomedito <chcchc1995@outlook.com>
2026-06-23 08:59:32 +0800

    ✨ feat(nvim):[#]update find files keymap

--- init.lua
+++ init.lua
@@ -877,18 +877,22 @@ add_lazy({
         pcall(require("telescope").load_extension, "ui-select")
 
         local builtin = require("telescope.builtin")
+        -- vim.keymap.set("n", "<leader>sf", builtin.find_files, {
+        --     desc = "[S]earch [F]iles",
+        -- })
+        -- vim.keymap.set("n", "<C-p>", builtin.find_files, {
+        --     desc = "[S]earch [F]iles",
+        -- })
+        vim.keymap.set("n", "<C-p>", function()
+            require("telescope.builtin").find_files({ cwd = vim.fn.getcwd() })
+        end, { desc = "Search Files" })
+
         -- vim.keymap.set("n", "<leader>sh", builtin.help_tags, {
         --     desc = "[S]earch [H]elp",
         -- })
         -- vim.keymap.set("n", "<leader>sk", builtin.keymaps, {
         --     desc = "[S]earch [K]eymaps",
         -- })
-        -- vim.keymap.set("n", "<leader>sf", builtin.find_files, {
-        --     desc = "[S]earch [F]iles",
-        -- })
-        vim.keymap.set("n", "<C-p>", builtin.find_files, {
-            desc = "[S]earch [F]iles",
-        })
         -- vim.keymap.set("n", "<leader>ss", builtin.builtin, {
         --     desc = "[S]earch [S]elect Telescope",
         -- })
